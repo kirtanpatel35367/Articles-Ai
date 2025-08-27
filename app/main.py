f012c864b6c78e37ea436e1521caa0ddab3a1e06
Kirtan Patel <kp534422@gmail.com>
2025-08-27 23:50:48 +0530

    cron time change

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ def send_mail():
 # send_mail()
 fetch_news()
 scheduler = BackgroundScheduler(timezone=ZoneInfo("Asia/Kolkata"))  # Set scheduler tz to IST
-scheduler.add_job(run_daily_digest, "cron", hour=23, minute=50)      # 11:00 PM IST
+scheduler.add_job(run_daily_digest, "cron", hour=23, minute=55)      # 11:00 PM IST
 scheduler.start()
 
 @app.on_event("shutdown")
